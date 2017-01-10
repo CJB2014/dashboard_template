1d683526788ae1ea737e3ec649c4c6ca7954b3de
Clemence Burnichon <clemence.burnichon@wdmp.local>
2017-01-10 15:34:48 +0000

    added the hearder color

--- global.R
+++ global.R
@@ -2,3 +2,12 @@
 library(shinydashboard)
 library(shiny)
 library(shinyjs)
+library(RODBC)
+library(lubridate)
+
+
+setwd('//wdmpsvr01/RedirectedFolders/clemence.burnichon/My Documents/code_repo/dashboard_template')
+bank <- read.csv('bank-full.csv', header = T , sep = ';', stringsAsFactors = F)
+
+
+
--- ui.R
+++ ui.R
@@ -5,22 +5,22 @@
 #-------------------------------------------------------------------------------------------------------------------
 
 
-dashboardPage( skin = 'green', 
+dashboardPage( skin = 'blue', 
       
   
 #-------------------------------------------------------------------------------------------------------------------
 # header 
 #-------------------------------------------------------------------------------------------------------------------
 
-    dashboardHeader(title = 'My Dashboard'),
+    dashboardHeader(title = 'WDMP - Dashboard Solution', titleWidth = 450),
  
 #-------------------------------------------------------------------------------------------------------------------
 # sidebar
 #-------------------------------------------------------------------------------------------------------------------
 
    dashboardSidebar(
-     sidebarSearchForm(textId = 'search', buttonId = 'search_button', label = 'Search...')
-    ,sidebarMenu(
+    # sidebarSearchForm(textId = 'search', buttonId = 'search_button', label = 'Search...'),
+    sidebarMenu(
        menuItem('Dashboard', tabName = 'Dashboard', icon = icon('dashboard'))
       ,menuItem('About', tabName = 'About', icon = icon('info'))
       ,dateRangeInput(inputId = 'date_range', label = 'Date Range:')
@@ -32,13 +32,59 @@ dashboardPage( skin = 'green',
 # Body 
 #-------------------------------------------------------------------------------------------------------------------
 
-  dashboardBody(
-    fluidRow(
+  dashboardBody(tags$head(tags$style(HTML('
+                                          /* logo */
+                                          .skin-blue .main-header .logo {
+                                          background-color: #00ABC2; color: #000000;
+                                          }
+                                          
+                                          /* logo when hovered */
+                                          .skin-blue .main-header .logo:hover {
+                                          background-color: #AAB7B8;
+                                          }
+                                          
+                                          /* navbar (rest of the header) */
+                                          .skin-blue .main-header .navbar {
+                                          background-color: #00ABC2;
+                                          }        
+                                          
+                                          /* main sidebar */
+                                          .skin-blue .main-sidebar {
+                                          background-color: #00ABC2;
+                                          }
+                                          
+                                          /* active selected tab in the sidebarmenu */
+                                          .skin-blue .main-sidebar .sidebar .sidebar-menu .active a{
+                                          background-color: #00ABC2; color:#000000;
+                                          }
+                                          
+                                          /* other links in the sidebarmenu */
+                                          .skin-blue .main-sidebar .sidebar .sidebar-menu a{
+                                          background-color: #00ABC2;
+                                          color: #000000;
+                                          }
+                                          
+                                          /* other links in the sidebarmenu when hovered */
+                                          .skin-blue .main-sidebar .sidebar .sidebar-menu a:hover{
+                                          background-color: #AAB7B8;
+                                          }
+                                          /* toggle button when hovered  */                    
+                                          .skin-blue .main-header .navbar .sidebar-toggle:hover{
+                                          background-color: #AAB7B8;
+                                          }
+                                          ')))
+                ,tags$head(tags$link(rel = 'stylesheet', type = 'text/css',href = 'custom.css'))
+    ,fluidRow(
      
     tabItems(
     
 ## Tab  1 
-       tabItem(tabName = 'Dashboard', h2('content dashboard'))
+       tabItem(tabName = 'Dashboard',
+                valueBox(value = dim(bank)[1], subtitle = 'Number of Customers contacted:', icon = icon('user'), color = 'yellow')
+               , box(title = 'box1', footer = 'footer_message' , solidHeader = T, collapsible = T )
+               , box(title = 'box2', footer = 'footer_message' , solidHeader = T, collapsible = T )
+               , box(title = 'box3', footer = 'footer_message' , solidHeader = T, collapsible = T )
+               )
     
     
 ## Tab 2     
